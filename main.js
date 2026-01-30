0d3dcc3cf6d7b68b50786a027b6deef958d3d526
KovalyovNikolay <39222877+KovalyovNikolay@users.noreply.github.com>
2026-01-30 17:28:53 +0500

    Скрипт расчета времени в минутах
    Работает по нажатию кнопки из HTML
    В HTML должны быть соответствующие элементы ввода - поиск по getElementById.

---
+++ main.js
@@ -0,0 +1,33 @@
+
+function GetValue() {
+	// Получаем значения
+    const hInput = document.getElementById("isHours");
+    const mInput = document.getElementById("isMinutes");
+
+    // Если элементы не найдены, выходим, чтобы не было ошибок
+    if (!hInput || !mInput) return;
+
+    const aHours = parseInt(hInput.value) || 0;
+    const aMinutes = parseInt(mInput.value) || 0;
+
+    // Условие: игнорировать, если оба значения 0
+    if (aHours === 0 && aMinutes === 0) {
+        console.log("Пропуск: значения равны нулю");
+        return; 
+		// Функция просто завершается, ничего не возвращая
+    }
+
+    // Расчет (перевод в десятичную долю суток)
+    const totalMinutes = (aHours * 60) + aMinutes;
+    const aFactor = 1 / (24 * 60); 
+    const result = totalMinutes * aFactor;
+
+    // Округление до 2 знаков
+    const finalValue = result.toFixed(2);
+
+    // Выводим результат через сообщение Алерт
+    alert(finalValue);
+    
+	
+    return finalValue;
+}
